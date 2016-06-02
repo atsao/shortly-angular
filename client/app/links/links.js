a68f813028ee5829c91568671fea22b9cc59db3c
Andrew Tsao <andrew.tsao8@gmail.com>
2016-06-02 12:30:46 -0700

    Added base templates and routes

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,13 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {
+    links: []
+  }
+  
+  $scope.getLinks = function() {
+    return $scope.data.links;
+  }
+
+  $scope.getLinks();
 });

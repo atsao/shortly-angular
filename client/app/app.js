8e245255222d3002e3d1efcfaa56c1baf0466a3d
Andrew Tsao <andrew.tsao8@gmail.com>
2016-06-04 12:14:34 -0700

    Fixed visit count incrementing twice, styling adjustments

--- client/app/app.js
+++ client/app/app.js
@@ -25,7 +25,7 @@ angular.module('shortly', [
       templateUrl: 'app/shorten/shorten.html',
       controller: 'ShortenController'
     })
-    .otherwise({ redirectTo: '/signin'});
+    .otherwise({ redirectTo: '/links'});
 
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
